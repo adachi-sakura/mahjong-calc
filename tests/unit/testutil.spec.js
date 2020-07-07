db5c8e77d37e229f19bac9aa42a38f3a9c9ea168
Adachi Sakura <805983409@qq.com>
2020-07-07 13:49:24 +0800

    test(context): add create context test

---
+++ tests/unit/testutil.spec.js
@@ -0,0 +1,27 @@
+import NewContext from '../../src/lib/testutil'
+
+describe('New Context test', () => {
+  it('create right context', () => {
+    const ctx = NewContext(
+      [
+        '1s',
+        '1s',
+        '1s',
+        '2s',
+        '2s',
+        '2s',
+        '3s',
+        '3s',
+        '3s',
+        '4s',
+        '4s',
+        '4s',
+        '5s',
+      ],
+      [],
+      '5s'
+    )
+    expect(ctx.tehai.length).toBe(13)
+    expect(ctx.fuuro.length).toBe(0)
+  })
+})
