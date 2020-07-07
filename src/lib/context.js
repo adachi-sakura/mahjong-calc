25446152e4c94d1ac72fe5649afef7da7a5dddac
Adachi Sakura <805983409@qq.com>
2020-07-07 09:13:03 +0800

    feat(context): add context module

---
+++ src/lib/context.js
@@ -0,0 +1,21 @@
+class Context {
+  constructor() {
+    this.tehai = []
+    this.fuuro = []
+    this.agariCard = {}
+    this.jifu = {}
+    this.chanfu = {}
+    this.tsumo = false
+    this.riichi = false
+    this.wriichi = false
+    this.ippatsu = false
+    this.chankan = false
+    this.haitei = false
+    this.hotei = false
+    this.linshan = false
+    this.tenho = false
+    this.chiho = false
+  }
+}
+
+export { Context }
