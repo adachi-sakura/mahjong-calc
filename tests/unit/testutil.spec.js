646fbc64f4863e087c845a3a18fcbcda24106d68
Adachi Sakura <805983409@qq.com>
2020-07-08 23:20:11 +0800

    test(util): test NewContext

--- tests/unit/testutil.spec.js
+++ tests/unit/testutil.spec.js
@@ -24,4 +24,16 @@ describe('New Context test', () => {
     expect(ctx.tehai.length).toBe(13)
     expect(ctx.fuuro.length).toBe(0)
   })
+  it('invalid card string context should be null', () => {
+    const ctx = NewContext(['1s', '0s', '10s'])
+    expect(ctx).toBeNull()
+  })
+  it('invalid fuuro context should be null', () => {
+    const ctx = NewContext(
+      ['1s', '1s', '1s', '2s', '2s', '2s', '5s'],
+      [['3s', '3s', '3s', '4s', '4s', '4s']],
+      '5s'
+    )
+    expect(ctx).toBeNull()
+  })
 })
